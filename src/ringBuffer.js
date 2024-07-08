08b1daf5f7130b2bc0a4cc009ef1b565be81f36f
tetta maeda <nu.maeda@gmail.com>
2024-07-08 17:15:45 +0900

    refactor: delete debug log

--- src/ringBuffer.js
+++ src/ringBuffer.js
@@ -22,11 +22,6 @@ class RingBuffer {
         const remainingSpace = this.length - this.rptr;
         const endIndex = Math.min(remainingSpace, readLength);
 
-        // console.log("this.data: " + this.data);
-        // console.log("this.rptr: " + this.rptr);
-        // console.log("endIndex: " + endIndex);
-        // console.log("isFull: " + this.isFull);
-
         // バッファが末尾に到達する前の部分を読み込む
         values.set(new this.arrayType(this.data.subarray(this.rptr, this.rptr + endIndex)));
 
@@ -40,9 +35,6 @@ class RingBuffer {
         this.rptr = (this.rptr + readLength) % this.length;
 
         this.isFull = false;
-
-        // console.log("values: " + values);
-
         return values;
         
     }
@@ -67,19 +59,13 @@ class RingBuffer {
         // wptrがbufferの範囲外の場合は循環させる
         this.wptr = (this.wptr + values.length) % this.length;
 
-        // console.log("this.wptr: " + this.wptr);
-
         const isPassing = () => {
             let isPassingWithoutCycle = this.wptr >= this.rptr && 
                                         this.wptr - this.rptr < values.length;
             let isPassingWithCycle = this.wptr < this.rptr && 
                                      this.length - this.rptr + this.wptr < values.length;
-            // console.log("isPassingWithoutCycle: " + isPassingWithoutCycle);
-            // console.log("isPassingWithCycle: " + isPassingWithCycle);
             return isPassingWithoutCycle || isPassingWithCycle;
         };
-
-        // console.log("isPassing: " + isPassing());
 
         // wptrがrptrを追い越した場合のみrptrの値を更新する
         if (isPassing()) {
